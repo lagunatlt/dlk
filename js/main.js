4aa8315971d2b358f1fb4fe654571cbbb8bf22e3
lagunatlt <lagunatlt@yandex.ru>
2020-01-01 04:53:36 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -9,8 +9,17 @@ $('.cat-slider__item').slick({
 	prevArrow: $('.button__prev'),
 	nextArrow: $('.button__next'),
 });
-
-// $('.cat-slider__item').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-// 	var i = (currentSlide ? currentSlide : 0) + 1;
-// 	$('.pagingInfo').text(i + '/' + slick.slideCount);
+// $('.main__item-slider').slick({
+// 	infinite: false,
+// 	slidesToShow: 1,
+// 	slidesToScroll: 1,
+// 	variableWidth: false,
+// 	centerMode: false,
+// 	arrows: false,
+// 	dots: true,
+// 	cssEase: 'linear',
+// });
+// $('.main__item-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+// 	let i = (currentSlide ? currentSlide : 0) + 1;
+// 	$('.dots-nav__current-slide').text("0" + i);
 // });
