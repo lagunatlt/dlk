3bf09faca638ec4c0229092fcb1a993566934e69
lagunatlt <lagunatlt@yandex.ru>
2020-01-02 22:53:07 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 $('.cat-slider__item').slick({
-	// infinite: true,
+	infinite: true,
 	slidesToShow: 5,
 	slidesToScroll: 4,
 	variableWidth: true,
 	centerMode: true,
-	// autoplay: true,
+	autoplay: true,
 	autoplaySpeed: 2000,
 	prevArrow: $('.button__prev'),
 	nextArrow: $('.button__next'),
 });
-// $('.main__item-slider').slick({
-// 	infinite: false,
-// 	slidesToShow: 1,
-// 	slidesToScroll: 1,
-// 	variableWidth: false,
-// 	centerMode: false,
-// 	arrows: false,
-// 	dots: true,
-// 	cssEase: 'linear',
-// });
-// $('.main__item-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-// 	let i = (currentSlide ? currentSlide : 0) + 1;
-// 	$('.dots-nav__current-slide').text("0" + i);
-// });
+$('.main__item-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	variableWidth: false,
+	centerMode: false,
+	arrows: false,
+	// dots: true,
+	// fade: true,
+	cssEase: 'linear',
+	// dotsClass: 'dots-nav__dots',
+});
+$('.main__item-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+	let i = (currentSlide ? currentSlide : 0) + 1;
+	$('.dots-nav__current-slide').text("0" + i);
+});
