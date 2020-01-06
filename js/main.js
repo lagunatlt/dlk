56239af4727d7224832cb8dd35b8aa1ba66707a8
lagunatlt <lagunatlt@yandex.ru>
2020-01-06 17:06:35 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+$(document).ready(function ($) {
+	let nav = $('.top_box');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 10) {
+			nav.addClass("top_box_shadow");
+		} else {
+			nav.removeClass("top_box_shadow");
+		}
+	});
+
+});
+
+
 $('.cat-slider__item').slick({
 	infinite: true,
 	slidesToShow: 5,
@@ -28,4 +41,35 @@ $('.main__item-slider').on('afterChange', function (event, slick, currentSlide, 
 	$('.dots-nav__current-slide').text("0" + i);
 });
 
-$('input[type="range"]').rangeslider('update', true);
+$('input[type="range"]').rangeslider({
+
+	polyfill: false,
+
+	rangeClass: 'rangeslider',
+	disabledClass: 'rangeslider--disabled',
+	horizontalClass: 'rangeslider--horizontal',
+	fillClass: 'rangeslider__fill',
+	handleClass: 'rangeslider__handle',
+
+	onInit: function () {
+		$rangeEl = this.$range;
+		let $handle = $rangeEl.find('.rangeslider__handle');
+		let handleValue = '<div class="rangeslider__handle__value">' + this.value + '</div>';
+		$handle.append(handleValue);
+
+		let rangeLabels = this.$element.attr('labels');
+		rangeLabels = rangeLabels.split(', ');
+
+		$rangeEl.append('<div class="rangeslider__labels"></div>');
+		$(rangeLabels).each(function (index, value) {
+			$rangeEl.find('.rangeslider__labels').append('<span class="rangeslider__labels__label">' + value + '</span>');
+		})
+	},
+
+	onSlide: function (position, value) {
+		let $handle = this.$range.find('.rangeslider__handle__value');
+		$handle.text(this.value);
+	},
+
+	onSlideEnd: function (position, value) { }
+});
