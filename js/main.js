9f8d83f9230f843062689e002473e3728105e16f
lagunatlt <lagunatlt@yandex.ru>
2020-01-07 23:17:40 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 $(document).ready(function ($) {
-	let nav = $('.top_box');
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 52) {
-			nav.addClass('fixed-top');
+			$('.top_box').addClass('fixed-top');
 		} else {
-			nav.removeClass('fixed-top');
+			$('.top_box').removeClass('fixed-top');
 		}
 	});
-
 });
 
+$(window).width(function () {
+	let width = $('body').innerWidth();
+		if (width <= 640) {
+			
+			$('.header__nav-wrap').addClass('fly_menu');
+		}
+});
+
+$('.menu_button-close').click(function () {
+	$('.header__nav-wrap').addClass('menu-hide')
+	$('body').removeClass('scroll-none')
+	$('.header__nav-wrap').removeClass('menu-open')
+})
+$('.menu_button-open').click(function () {
+	$('.header__nav-wrap').addClass('menu-open')
+	$('body').addClass('scroll-none')
+	$('.header__nav-wrap').removeClass('menu-hide')
+})
 
 $('.cat-slider__item').slick({
 	infinite: true,
